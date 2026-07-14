f5cd725ff41d7544b62e4224cda2aca66b740b92
Chantel <chantelboiro.dev@outlook.com>
2026-07-14 10:00:39 +0100

    animations added to start

--- src/components/About_Me.tsx
+++ src/components/About_Me.tsx
@@ -1,8 +1,10 @@
+import { useState } from 'react';
 import image from '../assets/portfolio-Img.jpg'
 import {motion} from 'framer-motion'
 import type { AboutMeProps } from '../types/about-me'
 
 export default function About_Me({isScrolled}: AboutMeProps) {
+    const [isFirstLoad, setIsFirstLoad] = useState(true);
   return (
     <>
     <div className="profile-img" style={{height: '350px', display: 'flex'}}>
@@ -13,11 +15,27 @@ export default function About_Me({isScrolled}: AboutMeProps) {
                 id="main-Img" 
                 src={image} 
                 alt="My profile"
-                transition={{
-                    type: "spring",
-                    stiffness: 300,
-                    damping: 30,
-                    mass: 1
+                initial={isFirstLoad ? {opacity: 0, scale: 0.5} : false}
+                animate={{opacity: 1, scale: 1}}
+                onAnimationComplete={() => setIsFirstLoad(false)}
+                transition={isFirstLoad ? {
+                    duration: 3,
+                    delay: 5,
+                    ease: [0, 0.71, 0.2, 1.01],
+
+                    layout: {
+                        type: "spring",
+                        stiffness: 300,
+                        damping: 30,
+                        mass: 1
+                    }
+                } : {
+                    layout: {
+                        type: "spring",
+                        stiffness: 250,
+                        damping: 30,
+                        mass: 1
+                    }
                 }}
             />
         )}
@@ -26,7 +44,11 @@ export default function About_Me({isScrolled}: AboutMeProps) {
     
      <div >
        
-        <h2 className='header'>About Me</h2>
+        <motion.h2 className='header'
+            // initial="hidden"
+            // whileInView="visible"
+            // viewport={{once: true}}
+        >About Me</motion.h2>
         <p className='text-experience'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa, voluptates tempore earum eveniet, ratione fuga explicabo exercitationem omnis, qui mollitia cum eius. Facere quas velit eos esse magnam doloribus obcaecati quibusdam tenetur, laborum cumque, dolor recusandae asperiores maiores provident nihil ducimus, sit alias quae? Quae, velit! Illum libero rerum ullam consequatur dolores eos veniam, enim ratione labore iste quos deserunt inventore, in deleniti vitae? Quae laudantium itaque quaerat excepturi commodi, repellat optio molestiae . Laborum, consequuntur!</p>
     </div>
     </>
